98aaaa9890ddf80d35aef397dd23bbf38e9e6dff
gautamsen117 <gautamsen117@gmail.com>
2022-09-14 23:56:04 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,25 @@
+function bringSingara(money){
+    console.log('Mama singara den');
+    console.log(money);
+}
+// bringSingara(100);
+
+function bringSingara(money){
+    console.log('taka dichen: ', money);
+    console.log('Ai nen singara');
+}
+var taka = 200;
+// bringSingara(taka);
+
+function add(num1, num2){
+    console.log('going to add: ', num1, num2)
+}
+
+// add(125, 94);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(1, 2, 3, 4, 5);
