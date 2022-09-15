a8453800fff3989976866021827488452a89d248
gautamsen117 <gautamsen117@gmail.com>
2022-09-15 13:30:04 +0600

    object

---
+++ function-problem-solving.js
@@ -0,0 +1,9 @@
+function gettotal(day1, day2){
+    const sum = day1 + day2;
+    return sum;
+    
+}
+
+var day1work = gettotal(10, 10);
+var day2work = gettotal(10, 10);
+var finalWork = gettotal(day1work, day2work);
---
+++ object.js
@@ -0,0 +1,22 @@
+var student = {
+    id: 001,
+    name: 'Gautam Sen',
+    height: '5.6Inc'
+
+}
+var mobile = {
+    brand: 'Samsung',
+    storage: '64gb',
+    price: 12000,
+    camera: '7MP'
+}
+
+var myComputer = {
+    brand: 'Lenevo',
+    price: 60000,
+    color: 'black',
+    proccessor: 'i5'
+}
+console.log(myComputer);
+myComputer.proccessor = 'i7';
+console.log(myComputer);
